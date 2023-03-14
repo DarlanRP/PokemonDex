b46dbb06591f29694883d42d8053949514b92e53
DarlanRP <darlanrodriguesp97@hotmail.com>
2023-03-14 19:27:34 -0300

    final

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import { DotWave } from '@uiball/loaders';
+import logo from '../assets/pikachuCorrendo.gif';
+
+export const Loader = () => {
+	return (
+        <div className="container-loader">
+            <img src={logo} alt="" />
+        </div>
+    )
+};
+
